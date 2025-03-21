57654b5cbb37d1658389c89d72a968e658d98afa
Tran Hoang Phuc <phucth115.dev@gmail.com>
2025-03-21 08:08:00 +0700

    fix edit profile

--- src/components/EditProfile.jsx
+++ src/components/EditProfile.jsx
@@ -13,18 +13,39 @@ export default function EditProfile({ user }) {
   const [imageList, setImageList] = useState([]);
   const [statusEditor, setStatusEditor] = useState(true);
   const [details, setDetails] = useState(user.aboutMe);
-
   const [displayName, setDisplayName] = useState(user.displayName);
   const [location, setLocation] = useState(user.location);
   const [title, setTitle] = useState(user.title);
-
+  const [isPopupOpen, setIsPopupOpen] = useState(false);
+  const fileInputRef = useRef(null);
+  const [file, setFile] = useState(null);
   const getImagesFromHTML = (html) => {
     const tempDiv = document.createElement("div");
     tempDiv.innerHTML = html;
     const imgs = tempDiv.getElementsByTagName("img");
     return Array.from(imgs).map((img) => img.src);
   };
-
+  const handleFileChange = async (event) => {
+    const file = event.target.files[0];
+    if (!file) {
+      return;
+    }
+    const validImageTypes = [
+      "image/jpeg",
+      "image/png",
+      "image/gif",
+      "image/webp",
+    ];
+    if (!validImageTypes.includes(file.type)) {
+      toast.error("Invalid image type", {
+        position: "bottom-left",
+        successClassName: "toast-error",
+        autoClose: 5000,
+      });
+      return;
+    }
+    setFile(file);
+  };
   const uploadImage = async (file, order) => {
     var formData = new FormData();
     formData.append("file", file);
@@ -135,13 +156,50 @@ export default function EditProfile({ user }) {
       );
       if (response.data && response.data.data) {
         setUser(response.data.data);
-        toast.success("Update succesfully", {
-          position: "bottom-left",successClassName: "toast-success",
-          autoClose: 5000,
-        },{autoClose: 5000});
+        toast.success(
+          "Update succesfully",
+          {
+            position: "bottom-left",
+            successClassName: "toast-success",
+            autoClose: 5000,
+          },
+          { autoClose: 5000 }
+        );
       }
     } catch (error) {
       console.error("Lỗi khi cập nhật profile:", error);
+    }
+  };
+
+  const handleImageSaveChange = () => {
+    if (file) {
+      var formData = new FormData();
+      formData.append("image", file);
+
+      try {
+        axios
+          .put(`${BaseUrl.uri}/profile/image`, formData, {
+            headers: {
+              Authorization: `Bearer ${token}`,
+            },
+          })
+          .then((response) => {
+            if (response.data && response.data.data) {
+              console.log(response.data.data);
+              // setUser(response.data.data);
+              // toast.success("Update succesfully", {
+              //   position: "bottom-left",successClassName: "toast-success",
+              //   autoClose: 5000,
+              // });
+            }
+          });
+      } catch (error) {
+        toast.error("Error when uploading image", {
+          position: "bottom-left",
+          successClassName: "toast-error",
+          autoClose: 5000,
+        });
+      }
     }
   };
   return (
@@ -157,12 +215,37 @@ export default function EditProfile({ user }) {
             alt="Profile"
             className="w-24 h-24 rounded-lg border border-gray-300"
           />
-          <button className="mt-2 bg-gray-700 text-white px-3 py-1 text-sm rounded-md hover:bg-gray-800">
+          <input type="file" className="mt-2" hidden="true" id="imageProfile" />
+          <button
+            className="mt-2 bg-gray-700 text-white px-3 py-1 text-sm rounded-md hover:bg-gray-800"
+            onClick={() => setIsPopupOpen(true)}
+          >
             Change picture
           </button>
         </div>
       </div>
-
+      {isPopupOpen && (
+        <div className="fixed top-0 left-0 w-full h-full flex justify-center items-center bg-black bg-opacity-50">
+          <div className="bg-white p-4 rounded-md shadow-md w-96">
+            <h3 className="text-lg font-semibold mb-2">Select a new picture</h3>
+            <input type="file" ref={fileInputRef} onChange={handleFileChange} />
+            <div className="flex justify-end mt-4">
+              <button
+                className="mr-2 px-3 py-1 bg-blue-500 text-white rounded-md"
+                onClick={handleImageSaveChange}
+              >
+                Change
+              </button>
+              <button
+                className="mr-2 px-3 py-1 bg-gray-500 text-white rounded-md"
+                onClick={() => setIsPopupOpen(false)}
+              >
+                Cancel
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
       <div className="mb-4">
         <label className="block  font-medium">Display name</label>
         <input
@@ -217,18 +300,17 @@ export default function EditProfile({ user }) {
         </button>
       </div>
       <ToastContainer
-                    position="bottom-left"
-                    autoClose={5000}
-                    hideProgressBar={false}
-                    newestOnTop={false}
-                    closeOnClick={true}
-                    rtl={false}
-                    pauseOnFocusLoss
-                    draggable
-                    pauseOnHover
-                    theme="light"
-                  />
+        position="bottom-left"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick={true}
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+        theme="light"
+      />
     </div>
-    
   );
 }
